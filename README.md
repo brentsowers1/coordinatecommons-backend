cb239b7c49e747fc721a7caee08a01cad6fa18c2
Brent Sowers <brentsowers1@gmail.com>
2020-05-17 12:06:23 -0400

    savePlace lambda updated to save to DynamoDB

--- README.md
+++ README.md
@@ -2,9 +2,8 @@ This is the back end code for the Coordinate Commons application (https://www.co
 
 It's still in the very early stages. Read https://www.coordinatecommons.com/app and click About in the top bar for more infomation on what Coordinate Commons is.
 
-From a technical perspective - this repo is a collection of lambda functions for saving/retrieving data for the coordinate commons application. Right now, there's just one lambda that doesn't save, but I'll be adding saving to DynamoDB soon. Users and login sessions are managed with AWS Cognito. The lambdas are called through API Gateway with a Cognito Authorizor in the middle.  The front end is a lot further along than this back end. The front end is a React application that uses Google Maps and calls the lambdas here. See the front end code at https://github.com/brentsowers1/coordinatecommons-frontend.
+From a technical perspective - this repo is a collection of lambda functions for saving/retrieving data for the coordinate commons application. Users and login sessions are managed with AWS Cognito. The lambdas are called through API Gateway with a Cognito Authorizor in the middle. There is no local deployment, for now I am just manually setting things up in the AWS console, and copying/pasting the lambda code in the AWS editor. (SAM coming soon) The front end is a React application that uses Google Maps and calls the lambdas here. See the front end code at https://github.com/brentsowers1/coordinatecommons-frontend.
 
 I'm doing this to get more hands on coding experience with React, and more hands on experience with lambdas, node, and AWS technologies. I am a software engineering manager, and have written much code in my years. In the past 5+ years I've managed teams that are building functionality in React, and have managed teams that are doing a few things with lambdas. But I only have a little direct hands on experience coding myself with React, and haven't build any lambdas myself, so I'm doing this project to learn more about those technologies, and to have some fun coding! 
 
 To read more about me, visit https://www.coordinatecommons.com.
-
--- lambdas/savePlace.js
+++ lambdas/savePlace.js
@@ -1,11 +1,15 @@
-// Note that this does NOT yet deploy... I just put the code in the lambda editor. I need to work on a deployment system.
-// And it does NOT yet save to a database. I want to hook this up to DynamoDB. But for now all that it does is return the 
-// logged in user name and the passed in place ID. The validation that you're authenticated happens through API gateway
-// having an authorizor configured to a cognito pool.
+// This is the lambda to save a place that you've visited, or unvisited. It gets called from the front end. It uses a 
+// Cognito UserPool authorizer (configured through the API gateway), so when this is called, the user is authenticated
+// and the user details are passed in. It writes or updates a DynamoDB document for the user with all of their 
+// places visited.
 
-// To call this lambda, you must pass a header of Authorization: token
-// where token is the Cognito ID token's jwt token value. You can get this through a login call through cognito. Or snoop
-// on the response when logging in to https://www.coordinatecommons.com/app
+// Note that this does NOT yet deploy... I just put the code in the lambda editor. I tried using SAM and Cloudformation for
+// deploying, but put this on hold for now to focus on getting application functionality working. The inline editor, and
+// manually configuring everything through AWS console works fine while my application is very small.
+
+const AWS = require('aws-sdk');
+
+const ddb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = (event, context, callback) => {
   if (!event.requestContext.authorizer) {
@@ -14,26 +18,72 @@ exports.handler = (event, context, callback) => {
   }
   
   const username = event.requestContext.authorizer.claims['cognito:username'];
+  const userId = event.requestContext.authorizer.claims['sub'];
   const requestBody = JSON.parse(event.body);
   if (!requestBody.placeId) {
     errorResponse('No place ID specified in request', context.awsRequestId, callback);
     return;
   }
+  if (!requestBody.placeType) {
+    errorResponse('No place type specified in request', context.awsRequestId, callback);
+    return;
+  }
   
+  getPlacesVisited(userId, requestBody.placeType).then((data) => {
+    if (!data || !data.Item || !data.Item.UserId) {
+      if (!requestBody.visited) {
+        console.log('No document, but the request is to not visit the place, so nothing to do');
+        successResponse(username, callback);
+      } else {
+        console.log('No document exists, writing a new one');
+        writeNewPlacesVisitedDocument(userId, requestBody.placeType, requestBody.placeId).then((data) => {
+          console.log('Wrote new document');
+          successResponse(username, callback);
+        }).catch((err) => {
+          console.error(err);
+          errorResponse('Error writing new document', context.awsRequestId, callback);
+        });
+      }
+    } else {
+      const placeInDocument = data.Item.PlacesVisited.find(pv => pv.Id === requestBody.placeId);
+      if ( (requestBody.visited && placeInDocument) || (!requestBody.visited && !placeInDocument)) {
+        console.log('Didn\'t need to write document');
+        successResponse(username, callback);
+      } else {
+        if (requestBody.visited) {
+          data.Item.PlacesVisited.push({Id: requestBody.placeId});
+        } else {
+          data.Item.PlacesVisited = data.Item.PlacesVisited.filter(pv => pv.Id !== requestBody.placeId);
+        }
+        updatePlacesVisitedDocument(data.Item).then((rsp) =>{
+          console.log('Successfuly updated document');
+          successResponse(username, callback);
+        }).catch((err) => {
+          console.error(err);
+          errorResponse('Error updating document', context.awsRequestId, callback);
+        });
+      }
+    }
+  }).catch((err) => {
+    console.error(err);
+    errorResponse('Error getting document', context.awsRequestId, callback);
+  });
+};
+
+const successResponse = (username, callback) => {
   // do the actual saving
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({
-      username: username,
-      placeId: requestBody.placeId
+      username: username
     }),
     headers: {
       'Access-Control-Allow-Origin': '*',
     }
-  });
+  });  
 };
 
-function errorResponse(errorMessage, awsRequestId, callback) {
+const errorResponse = (errorMessage, awsRequestId, callback) => {
   callback(null, {
     statusCode: 500,
     body: JSON.stringify({
@@ -44,5 +94,38 @@ function errorResponse(errorMessage, awsRequestId, callback) {
       'Access-Control-Allow-Origin': '*',
     },
   });
-}
+};
 
+const getPlacesVisited = (userId, placeType) => {
+  return ddb.get({
+    TableName: 'PlacesVisited',
+    Key: {
+      UserId: userId,
+      PlaceType: placeType
+    },
+    ConsistentRead: true
+  }).promise();
+};
+
+const writeNewPlacesVisitedDocument = (userId, placeType, placeId) => {
+  return ddb.put({
+    TableName: 'PlacesVisited',
+    Item: {
+      UserId: userId,
+      PlaceType: placeType,
+      PlacesVisited: [{
+        Id: placeId
+      }],
+      CreatedAt: new Date().toISOString(),
+      UpdatedAt: new Date().toISOString()
+    }
+  }).promise();
+};
+
+const updatePlacesVisitedDocument = (newDocument) => {
+  newDocument.UpdatedAt = new Date().toISOString();
+  return ddb.put({
+    TableName: 'PlacesVisited',
+    Item: newDocument
+  }).promise();
+}
