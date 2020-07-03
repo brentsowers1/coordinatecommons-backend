6671c86f7268b3d6117a9cd543d570908a3d079a
Brent Sowers <brentsowers1@gmail.com>
2020-07-02 20:14:05 -0400

    Updated the get visits lambda to do a query for all place types if no place type query parameter is specified

--- lambdas/GetVisits/index.js
+++ lambdas/GetVisits/index.js
@@ -10,22 +10,36 @@ exports.handler = (event, context, callback) => {
   if (!userId) {
     errorResponse('No sub provided', context.awsRequestId, callback);
   }
-  const placeType = event.queryStringParameters.placeType;
-  getPlacesVisited(userId, placeType).then((data) => {
-    if (data && data.Item && data.Item.UserId) {
-      console.log('Got a document');
-      successResponse(data.Item.PlacesVisited, callback);
-    } else {
-      console.log('No document found');
-      successResponse([], callback);
-    }
-  }).catch((err) => {
-    console.error(err);
-    errorResponse('Error getting visits', context.awsRequestId, callback);
-  })
+  const placeType = event.queryStringParameters ? event.queryStringParameters.placeType : null;
+  if (placeType) {
+    getPlacesVisitedByType(userId, placeType).then((data) => {
+      if (data && data.Item && data.Item.UserId) {
+        successResponse({
+          placesVisited: data.Item.PlacesVisited
+        }, callback);
+      } else {
+        console.log('No document found');
+        successResponse({placesVisited: []}, callback);
+      }
+    }).catch((err) => {
+      console.error(err);
+      errorResponse('Error getting visits', context.awsRequestId, callback);
+    });
+  } else {
+    getAllPlacesVisited(userId).then((data) => {
+      if (data && data.Items) {
+        successResponse({places: data.Items}, callback);
+      } else {
+        successResponse({places: []}, callback);
+      }
+    }).catch((err) => {
+      console.error(err);
+      errorResponse('Error getting visits', context.awsRequestId, callback);
+    });
+  }
 };
 
-const getPlacesVisited = (userId, placeType) => {
+const getPlacesVisitedByType = (userId, placeType) => {
   const key = {
     UserId: userId,
     PlaceType: placeType
@@ -37,12 +51,23 @@ const getPlacesVisited = (userId, placeType) => {
   }).promise();
 };
 
-const successResponse = (placesVisited, callback) => {
+const getAllPlacesVisited = (userId) => {
+  return ddb.query({
+    TableName: 'PlacesVisited',
+    KeyConditionExpression: 'UserId = :userId',
+    ExpressionAttributeValues: {
+      ':userId': userId
+    },
+    ProjectionExpression: 'PlacesVisited, PlaceType',
+    ConsistentRead: true
+  }).promise();
+};
+
+
+const successResponse = (responseBody, callback) => {
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({
-      placesVisited: placesVisited
-    }),
+    body: JSON.stringify(responseBody),
     headers: {
       'Access-Control-Allow-Origin': '*',
     }
